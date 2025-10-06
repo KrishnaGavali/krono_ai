034a7015f0cc7bd590b3102f46fd02d6ef71dff6
Krishna Gavali <krishnagavali973@gmail.com>
2025-10-06 13:45:05 +0530

    feat: restructure PlanGrid component layout and add timeSlots array

--- frontend/components/Plan/PlanGrid.tsx
+++ frontend/components/Plan/PlanGrid.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const PlanGrid = () => {
+  const timeSlots = new Array();
+
   return (
-    <div className="flex-1">
-      <div className=" h-10 w-full border-b border-border sticky top-30">
-        Hello
+    <div className="w-full flex flex-col">
+      <div className=" h-10 w-full border-b border-border sticky top-30 text-center flex flex-col justify-center z-10 bg-background">
+        <span className=" font-medium">Monday, 30 Sep</span>
+      </div>
+      <div className=" flex-1 ">
+        {new Array(24).fill(0).map((_, index) => (
+          <div
+            key={index}
+            className=" h-10 w-full border-b border-border "
+          ></div>
+        ))}
       </div>
     </div>
   );
