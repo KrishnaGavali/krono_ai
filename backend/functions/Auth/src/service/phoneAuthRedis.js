20f0491a8db26b3612086d6c91368316eb0d55bd
Krishna Gavali <krishnagavali973@gmail.com>
2025-11-22 16:28:32 +0530

    feat: Implement phone authentication session management with Upstash Redis integration

---
+++ backend/functions/Auth/src/service/phoneAuthRedis.js
@@ -0,0 +1,44 @@
+import redis from '../providers/upstashRedis.js';
+
+class phoneAuthRedis {
+  redisClient = redis;
+
+  constructor() {}
+
+  async createAuthSession(code, userId, name, phone) {
+    try {
+      const sessionData = {
+        userId: userId,
+        name: name,
+        phone: phone,
+      };
+
+      const session_key = `phone_auth_code:${code}`;
+
+      await this.redisClient.set(session_key, JSON.stringify(sessionData), {
+        ex: 600,
+      });
+
+      return true;
+    } catch (error) {
+      throw new Error('Error creating auth session: ' + error.message);
+    }
+  }
+
+  async getAuthSession(code) {
+    try {
+      const session_key = `phone_auth_code:${code}`;
+      const sessionData = await this.redisClient.get(session_key);
+
+      if (!sessionData) {
+        return null;
+      }
+
+      return JSON.parse(sessionData);
+    } catch (error) {
+      throw new Error('Error retrieving auth session: ' + error.message);
+    }
+  }
+}
+
+export default phoneAuthRedis;
